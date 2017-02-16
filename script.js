d3994a289bf6a1d86cb60141177897d657b97f5d
Aaron Sikes <aaron@sikes.io>
2017-02-16 12:14:52 -0500

    Add an html fetch example.
    Don't include both fetch & xhr examples in one script, it's confusing

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
-var FETCH_METHOD = 'xhr'; // = 'fetch';
+var html = document.querySelector('#html');
+var pdf = document.querySelector('#pdf');
 
-var token = 'abcd';
+populateIframe(html, 'other.html', [['x-api-version', 'v1.2']]);
+populateIframe(pdf, 'peepcode-git.pdf', [['Authorization', 'Bearer abcd1234']]);
 
-if (FETCH_METHOD === 'fetch') {
-
-  req = fetch('peepcode-git.pdf', {
-    headers: {
-      Authorization: 'Bearer ' + token
-    }
-  }).then(function (response){
-      return response.blob();
-  }).then(setPDF);
-
-} else if (FETCH_METHOD === 'xhr') {
-
+function populateIframe(iframe, url, headers) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'peepcode-git.pdf');
+  xhr.open('GET', url);
   xhr.onreadystatechange = handler;
   xhr.responseType = 'blob';
-  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+  headers.forEach(function(header) {
+    xhr.setRequestHeader(header[0], header[1]);
+  });
   xhr.send();
 
   function handler() {
     if (this.readyState === this.DONE) {
       if (this.status === 200) {
         // this.response is a Blob, because we set responseType above
-        setPDF(this.response);
+        iframe.src = URL.createObjectURL(this.response);
       } else {
-        console.error('no pdf :(');
+        console.error('XHR failed', this);
       }
     }
   }
+
+
 }
-
-
-
 
 function setPDF(blob) {
   document.querySelector('#blob').src = URL.createObjectURL(blob);
